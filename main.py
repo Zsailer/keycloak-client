968e3b4fd3114d51eb3588619fae936b1e4f33a5
Zsailer <zachsailer@gmail.com>
2018-09-18 13:33:28 -0700

    Add general requests from admin user

--- main.py
+++ main.py
@@ -2,31 +2,41 @@ from urllib.parse import urljoin
 import requests as re
 from pprint import pprint
 import json
+from functools import wraps
 
-USER = "user1"
-PASSWORD = "password"
 
 # API Port
 PORT = 8080
 BASE_URL = f"http://localhost:{PORT}"
-HEADERS = {}
 
 
-REALM = "master"
-TOKEN_URL = f"auth/realms/{REALM}/protocol/openid-connect/token"
+def reauthorize(method):
+    """Refresh the user's auth_state."""
+    @wraps(method)
+    def inner(self, *args, **kwargs):
+        self.authorize_user()
+        return method(self, *args, **kwargs)
+    return inner
+
 
 class User(object):
     """KeyCloak user.
     """
-    def __init__(self, username, password, auth_state):
+    def __init__(self, 
+        username, 
+        password, 
+        realm="master",
+        auth_state={}
+        ):
+        self.realm = realm
         self.username = username
         self.auth_state = auth_state
         self.password = password
+        self.token_url = f"auth/realms/{self.realm}/protocol/openid-connect/token"
     
     @property
     def token(self):
         return self.auth_state["access_token"]
-
 
     def authorize_user(self):
         headers = {
@@ -40,7 +50,7 @@ class User(object):
         }
 
         r = re.post(
-            urljoin(BASE_URL, TOKEN_URL),
+            urljoin(BASE_URL, self.token_url),
             headers=headers,
             data=data
         )
@@ -50,14 +60,14 @@ class User(object):
 
         self.auth_state = r.json()
 
-    
+    @reauthorize
     def request(self, url="", verb="get", headers={}, data=None):
         """Build a custom request, print reply, 
         """
         operation = getattr(re, verb)
 
         # Build URL.
-        url = urljoin(BASE_URL, f"{url}")
+        url = urljoin(BASE_URL, f"auth/admin/realms/{self.realm}/{url}")
 
         # Update given headers with authorization token.
         headers.update({"Authorization": f"Bearer {self.token}"})
@@ -86,55 +96,7 @@ class User(object):
 
 
 def sign_in(username, password):
-
-    user = User(username, password, {})
+    """Sign in to Keycloak server using Admin credentials"""
+    user = User(username, password)
     user.authorize_user()
-
     return user
-
-    # token_url = urljoin(BASE_URL, "auth/realms/master/protocol/openid-connect/token")
-    # headers = {
-    #     "Content-Type": "application/x-www-form-urlencoded",
-    # }
-    # data = {
-    #     "username": username,
-    #     "password": password,
-    #     "client_id": "admin-cli",
-    #     "grant_type": "password"
-    # }
-
-    # r = re.post(
-    #     token_url, 
-    #     headers=headers,
-    #     data=data
-    # )
-
-    # return r
-
-# def keycloak_request(url="", verb="get", data=None):
-#     """Build a custom request, print reply, 
-#     """
-#     operation = getattr(re, verb)
-
-#     # Build URL.
-#     url = urljoin(BASE_URL, f"auth/{url}")
-#     # Print URL
-#     print(f"{verb.upper()} request to:\n{url}\n")
-
-#     # Make request
-#     r = operation(
-#         url, 
-#         headers=HEADERS,
-#         json=data
-#     )
-
-#     print(f"Status code: {r.status_code}\n")
-
-#     try: 
-#         out = r.json()
-#         print("JSON Response: ")
-#         pprint(out)
-#     except json.JSONDecodeError:
-#         print("Not valid JSON?\n")
-#         print(f"Reason: {r.reason}")
-#     return r
